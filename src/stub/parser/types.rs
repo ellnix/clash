e1d8c24fdf4caafebd8305d317132c0142a5ea28
ellnix <103502144+ellnix@users.noreply.github.com>
2024-03-12 19:00:10 +0100

    Rename object to count_var in Loopline

--- src/stub/parser/types.rs
+++ src/stub/parser/types.rs
@@ -103,7 +103,7 @@ pub enum Cmd {
         command: Box<Cmd>,
     },
     LoopLine {
-        object: String,
+        count_var: String,
         variables: Vec<VariableCommand>,
     },
     Write(String),
